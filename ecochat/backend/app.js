c3ea24fea106da10b365955465d557940f2cc0cd
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2026-06-09 17:49:53 +0530

    url updated

--- ecochat/backend/app.js
+++ ecochat/backend/app.js
@@ -11,7 +11,7 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:5173",
-    origin: "https://ecochat-git-main-sanjeetkumarraos-projects.vercel.app/",
+    origin: "https://ecochat-wheat.vercel.app/",
     credentials: true,
   },
 });
@@ -22,7 +22,7 @@ app.set("io", io);
 // Initialize socket handlers
 initSocket(io);
 
-app.use(cors({ origin: "https://ecochat-git-main-sanjeetkumarraos-projects.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://ecochat-wheat.vercel.app/", credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
